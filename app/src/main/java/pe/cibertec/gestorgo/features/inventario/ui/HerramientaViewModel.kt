31298f2c30aa871c7d9e1a2026a32f61d8f08072
Marco <marco_x1230@hotmail.com>
2025-06-07 11:40:31 -0500

    Agregando el viewmodel del inventario

---
+++ app/src/main/java/pe/cibertec/gestorgo/features/inventario/ui/HerramientaViewModel.kt
@@ -0,0 +1,10 @@
+package pe.cibertec.gestorgo.features.inventario.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HerramientaViewModel @Inject constructor(): ViewModel() {
+    private val ui :String = ""
+}
